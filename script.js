5157cf2c16b07d594afa6ae28b52caeb3239e689
EmilsonMendesPEM <emilson.mendes@escola.pr.gov.br>
2026-06-16 10:11:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const btnSaibaMais = document.getElementById("btnSaibaMais");
-
-btnSaibaMais.addEventListener("click", () => {
-    alert(
-        "A produção sustentável de tomate utiliza técnicas que preservam o solo, economizam água e reduzem o uso de produtos químicos."
-    );
-});
 
