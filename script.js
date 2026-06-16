91489a49b9c00ce9731ba48b80730437d3e6bf82
EmilsonMendesPEM <emilson.mendes@escola.pr.gov.br>
2026-06-16 10:11:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function mostrarSecao(id) {
+    let secoes = document.querySelectorAll('.secao');
 
+    secoes.forEach(secao => {
+        secao.classList.remove('ativa');
+    });
+
+    document.getElementById(id).classList.add('ativa');
+}
+
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "🌱 O futuro sustentável depende das escolhas que fazemos hoje no campo e na cidade!";
+}
